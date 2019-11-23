00b189c0c94664d0c9576cb5ee250f0571e0c213
Sebastian Vesterlund <aletheos@outlook.com>
2019-11-23 19:26:12 +0100

    Renamed objects

--- scripts/objects/Earth.gd
+++ scripts/objects/Earth.gd
@@ -3,5 +3,5 @@ extends KinematicBody
 const SECONDS_TO_TURN = 24
 
 func _process(delta):
-	$Sphere.rotate(Vector3.UP, delta * TAU / SECONDS_TO_TURN)
+	$Mesh.rotate(Vector3.UP, delta * TAU / SECONDS_TO_TURN)
 	pass
