669ce36770f82db474afe61a40d2c4e940480912
Sebastian Vesterlund <aletheos@outlook.com>
2019-11-23 21:41:06 +0100

    Continue work on brain code

--- scripts/Brain.gd
+++ scripts/Brain.gd
@@ -4,8 +4,8 @@ var callSentence = []
 var responseSentence = []
 
 func funcName(arg1:int, arg2:int) -> int:
-	var result = (arg1 + (arg1 * arg2)) / arg2 if arg2 == 0 else 1
-	return 0
+	var result : int = (arg1 + (arg1 * arg2)) / arg2 if arg2 != 0 else 1
+	return result
 
 func _ready():
 	pass
